c06e341ccded01b2af092db2c30f45e7ed5fb7cf
Steve Min <stevekmin@gmail.com>
2020-10-21 08:34:02 -0700

    modify get product function to select by ID add 404 conditional

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,16 +1,21 @@
 const Product = require('../models/productModel');
 
-async function getProducts(req, res) {
+async function getProduct(req, res, id) {
   try {
-    const products = await Product.findAll();
+    const product = await Product.findById(id);
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(products));
+    if (!product) {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Product Not Found' }));
+    } else {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(product));
+    }
   } catch (error) {
     console.log(error);
   }
 }
 
 module.exports = {
-  getProducts,
+  getProduct,
 };
