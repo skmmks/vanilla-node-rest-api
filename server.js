b3922fcf0252a2eded275905fe870ec016d02667
Steve Min <stevekmin@gmail.com>
2020-10-21 08:46:35 -0700

    add conditional for POST request

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const { getProducts, getProduct } = require('./controllers/productController');
+const { getProducts, getProduct, createProduct } = require('./controllers/productController');
 
 const server = http.createServer((req, res) => {
   if (req.url === '/api/products' && req.method === 'GET') {
@@ -7,6 +7,8 @@ const server = http.createServer((req, res) => {
   } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET') {
     const id = req.url.split('/')[3];
     getProduct(req, res, id);
+  } else if (req.url === '/api/products' && req.method === 'POST') {
+    createProduct(req, res);
   } else {
     res.writeHead(404, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Route Not Found' }));
