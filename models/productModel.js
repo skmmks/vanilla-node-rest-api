e53485d9bb43d89bfc4b07bbfb7ed2e4a25e4c67
Steve Min <stevekmin@gmail.com>
2020-10-22 09:22:50 -0700

    add create product function create new product with new id and passed in product controller params

--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,5 @@
 const products = require('../data/products.json');
+const { v4: uuidv4 } = require('uuid');
 
 function findAll() {
   return new Promise((resolve, reject) => {
@@ -10,6 +11,12 @@ function findByID(id) {
   return new Promise((resolve, reject) => {
     const product = products.find((p) => p.id === id);
     resolve(product);
+  });
+}
+
+function create(product) {
+  return new Promise((resolve, reject) => {
+    const newProduct = { id: uuidv4(), ...product };
   });
 }
 module.exports = {
