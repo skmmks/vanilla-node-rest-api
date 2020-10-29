e2dad65509e84dcad55ab87f1327ff14cba0bdec
Steve Min <stevekmin@gmail.com>
2020-10-29 08:55:25 -0700

    create update function skeleton

--- models/productModel.js
+++ models/productModel.js
@@ -24,6 +24,15 @@ function create(product) {
     resolve(newProduct);
   });
 }
+
+function update(id, product) {
+  return new Promise((resolve, reject) => {
+    const newProduct = { id: uuidv4(), ...product };
+    products.push(newProduct);
+    writeDataToFile('./data/products.json', products);
+    resolve(newProduct);
+  });
+}
 module.exports = {
   findAll,
   findByID,
