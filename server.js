74bf52e62de53fc3a50b661376f218c47c3419be
Steve Min <stevekmin@gmail.com>
2020-10-30 09:25:55 -0700

    add delete route to server file

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const server = http.createServer((req, res) => {
   } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT') {
     const id = req.url.split('/')[3];
     updateProduct(req, res, id);
+  } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE') {
+    const id = req.url.split('/')[3];
+    removeProduct(req, res, id);
   } else {
     res.writeHead(404, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Route Not Found' }));
