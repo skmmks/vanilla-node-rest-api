79294b704fc833c139096adb9e25d96089b901aa
Steve Min <stevekmin@gmail.com>
2020-10-30 09:26:54 -0700

    rename remove product to delete product import deleteProduct to server file

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const http = require('http');
-const { getProducts, getProduct, createProduct, updateProduct } = require('./controllers/productController');
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require('./controllers/productController');
 
 const server = http.createServer((req, res) => {
   if (req.url === '/api/products' && req.method === 'GET') {
@@ -14,7 +20,7 @@ const server = http.createServer((req, res) => {
     updateProduct(req, res, id);
   } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE') {
     const id = req.url.split('/')[3];
-    removeProduct(req, res, id);
+    deleteProduct(req, res, id);
   } else {
     res.writeHead(404, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Route Not Found' }));
